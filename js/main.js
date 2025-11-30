8ec7726d75dd650e36c2306b6a6d35742f5208f3
Santiago Ramirez <miguelsantiago1940@gmail.com>
2025-11-30 14:46:10 -0400

    globalized umami

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
+  (function () {
+    const script = document.createElement("script");
+    script.defer = true;
+    script.src = "https://cloud.umami.is/script.js";
+    script.setAttribute("data-website-id", "ce625abf-956a-4572-9c80-f5e651eb789d");
+    document.head.appendChild(script);
+  })();
+
   var titleText = document.querySelector("title").text;
   var repeat = true;
   var index = 0;

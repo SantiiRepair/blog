5abbdf9ed055d77630e568912afa7422dcb892f1
Santiago Ramirez <miguelsantiago1940@gmail.com>
2025-11-30 16:09:35 -0400

    chore: added a few films

--- js/main.js
+++ js/main.js
@@ -7,20 +7,19 @@ document.addEventListener("DOMContentLoaded", function () {
     document.head.appendChild(script);
   })();
 
-  var titleText = document.querySelector("title").text;
-  var repeat = true;
+  var text = document.title;
   var index = 0;
 
-  function scrollTitle() {
-    if (index <= titleText.length) {
-      document.title = titleText.substring(0, index);
+  function transform() {
+    if (index <= text.length) {
+      document.title = text.substring(0, index);
       index++;
-      setTimeout(scrollTitle, 200);
+      setTimeout(transform, 200);
     } else {
       index = 0;
-      if (repeat) setTimeout(scrollTitle, 1000);
+      setTimeout(transform, 1000);
     }
   }
 
-  scrollTitle();
+  transform();
 });
